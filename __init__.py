64afc3b660b45b4cf931b0a32b2f69892f2c18e0
jayanam <jayanam.games@gmail.com>
2018-01-25 21:20:15 +0100

    3.0.0 - Add immediate mode

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Fast Carve",
     "description": "Hardsurface utility Blender addon for quick and easy boolean and bevel operations",
     "author": "Jayanam",
-    "version": (0, 2, 2, 0),
+    "version": (0, 3, 0, 0),
     "blender": (2, 79, 0),
     "location": "View3D",
     "category": "Object"}
@@ -23,6 +23,8 @@ from . fc_immediate_mode_op import FC_Immediate_Mode_Operator
 
 bpy.types.Scene.carver_target = PointerProperty(type=bpy.types.Object)
 
+addon_keymaps = []
+
 def register():
    bpy.utils.register_class(FC_Panel)
    bpy.utils.register_class(FC_Bevel_Panel)
@@ -32,6 +34,13 @@ def register():
    bpy.utils.register_class(FC_BoolOperator_Union)
    bpy.utils.register_class(FC_BoolOperator_Slice)
    bpy.utils.register_class(FC_Immediate_Mode_Operator)
+   
+   # add keymap entry
+   kcfg = bpy.context.window_manager.keyconfigs.addon
+   if kcfg:
+       km = kcfg.keymaps.new(name='3D View', space_type='VIEW_3D')
+       kmi = km.keymap_items.new("object.fc_immediate_mode_op", 'F', 'PRESS', shift=True, ctrl=True)
+       addon_keymaps.append((km, kmi))
     
 def unregister():
    bpy.utils.unregister_class(FC_Panel)
@@ -41,6 +50,12 @@ def unregister():
    bpy.utils.unregister_class(FC_BoolOperator_Diff)
    bpy.utils.unregister_class(FC_BoolOperator_Union)
    bpy.utils.unregister_class(FC_BoolOperator_Slice)
+   
+   # remove keymap entry
+   for km, kmi in addon_keymaps:
+       km.keymap_items.remove(kmi)
+   addon_keymaps.clear()
+    
    bpy.utils.unregister_class(FC_Immediate_Mode_Operator)
     
 if __name__ == "__main__":
--- fc_immediate_mode_op.py
+++ fc_immediate_mode_op.py
@@ -1,23 +1,112 @@
 import bpy
 
-# TODO: Implement immediate mode
+# Blender utils and fonts module
+import blf
+
+# Blender wrapper for opengl
+import bgl
+
+from . fc_bool_util import execute_boolean_op, execute_slice_op
+
+def draw_bg(region):
+    # Enable Opengl alpha
+    bgl.glEnable(bgl.GL_BLEND)
+    
+    # set color: red, green, blue, alpha
+    bgl.glColor4f(1.0, 0.0, 0.0, 0.1)
+    
+    # draw rectangle
+    # x0, y0, x1, y1
+    bgl.glRecti(1, 100, region.width, 0)
+
+def create_font(id, size):
+    blf.size(id, size, 72)
+      
+def draw_text(text, x, y, font_id):
+
+    blf.position(font_id, x, y , 0)
+    
+    bgl.glColor4f(1.0, 1.0, 1.0, 1.0)
+    blf.draw(font_id, text)  
+        
+# Immediate mode operator
 class FC_Immediate_Mode_Operator(bpy.types.Operator):
-    bl_idname = "scene.fc_immediate_mode_op"
+    bl_idname = "object.fc_immediate_mode_op"
     bl_label = "Immediate Mode Operator"
     bl_description = ""
     bl_options = {"REGISTER"}
 
+    @classmethod
+    def poll(cls, context):
+        return True
+
     def invoke(self, context, event):
         args = (self, context)
-    
-        context.area.tag_redraw()
         
+        # Register draw callback
+        self._handle = bpy.types.SpaceView3D.draw_handler_add(self.draw_callback_px, args, "WINDOW", "POST_PIXEL")
+        
+        self._mode = 0
+        
+        context.window_manager.modal_handler_add(self)
         return {"RUNNING_MODAL"}
-
+    
     def modal(self, context, event):
-        return {'CANCELLED'}
+        context.area.tag_redraw()
+
+        if event.type in {"ESC"}:
+            return self.finish()
+        
+        # Change mode
+        if event.type == 'M' and event.value == 'PRESS':
+            self._mode = (self._mode + 1)  % 3
+            return {'RUNNING_MODAL'}
+        
+        # Execute 
+        if event.type  == 'SPACE' and event.value == 'PRESS':
+            target_obj = bpy.context.scene.carver_target
+            
+            if self._mode < 2:
+                execute_boolean_op(context, target_obj, self._mode)
+            elif self._mode == 2:
+                execute_slice_op(context, target_obj)
+                
+            return {'RUNNING_MODAL'}
+        
+        return {"PASS_THROUGH"}
 
     def finish(self):
+        bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
         return {"FINISHED"}
 
-    
+        
+    # Draw handler to paint onto the screen
+    def draw_callback_px(tmp, self, context):
+        
+        region = context.region
+        
+        draw_bg(region)
+            
+        xt = int(region.width / 2.0)
+                
+        if self._mode == 0:
+            mode = "Difference"
+        elif self._mode == 1:
+            mode = "Union"
+        else:
+            mode = "Slice"
+        
+        text = "Mode (M): {0}".format(mode)
+        
+        # Big font
+        font_id = 0
+        create_font(font_id, 25)
+        
+        # Small font 
+        s_font_id = 1
+        create_font(s_font_id, 10)
+        
+        draw_text(text, xt - blf.dimensions(font_id, text)[0] / 2, 60, font_id)
+        
+        text_info = "Apply: Space | Finish: Esc"
+        draw_text(text_info, xt- blf.dimensions(s_font_id, text_info)[0] / 2, 30, s_font_id)
