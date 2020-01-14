292aab51e263da4727a2df6f556397c1fa193ef5
jayanam <jayanam.games@gmail.com>
2020-01-14 18:35:21 +0100

    0.9.8.2 : Hide properties for curve primitive

--- fc_primitive_panel.py
+++ fc_primitive_panel.py
@@ -14,26 +14,26 @@ class FC_PT_Primitive_Panel(Panel):
         row = layout.row()
         layout.prop(context.scene, "primitive_type")
 
-        row = layout.row()
-        layout.prop(context.scene, "bool_mode")
-        
-        row = layout.row()
-        layout.prop(context.scene, "draw_distance")
+        if context.scene.primitive_type != "Curve":
+            row = layout.row()
+            layout.prop(context.scene, "bool_mode")
+            
+            row = layout.row()
+            layout.prop(context.scene, "draw_distance")
+
+            row = layout.row()
+            col = row.column()
+            col.prop(context.scene, "extrude_mesh", text="Extrude")
+            
+            col = row.column()
+            col.prop(context.scene, "fill_mesh", text="Fill")
+
+            row = layout.row()
+            col = row.column()
+            col.prop(context.scene, "use_snapping", text="Snap grid")
+
+            col = row.column()
+            col.prop(context.scene, "snap_to_target", text="Snap target")
 
         row = layout.row()
-        col = row.column()
-        col.prop(context.scene, "extrude_mesh", text="Extrude")
-        
-        col = row.column()
-        col.prop(context.scene, "fill_mesh", text="Fill")
-
-        row = layout.row()
-        col = row.column()
-        col.prop(context.scene, "use_snapping", text="Snap grid")
-
-        col = row.column()
-        col.prop(context.scene, "snap_to_target", text="Snap target")
-
-        row = layout.row()
-
         row.operator("object.fc_immediate_mode_op", text="Primitive Mode")
